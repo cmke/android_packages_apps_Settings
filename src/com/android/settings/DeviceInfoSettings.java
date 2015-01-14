6e25f0da945d675ac2801105a6aefb7b46dd3f99
Paul Beeler <pbeeler80@gmail.com>
2015-01-13 23:44:22 -0700

    SQUASHED gcc and gcc flags info
    Change-Id: I5a6e645d25f3f095ef29a18ba389a163c5ca6bda
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Disable more clicks for sm info
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Added SaberMod Android banner in About Phone
    
    Change-Id: Id23a1ac74b4a5eac0d2f57479780eb2d0e97de68
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	res/xml/device_info_settings.xml
    
    Conflicts:
    	res/xml/device_info_settings.xml
    
    Move sm banner down
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -84,6 +84,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_DEVICE_MEMORY = "device_memory";
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_SM_AND = "sm_android";
+    private static final String KEY_SM_KERNEL = "sm_kernel";
+    private static final String KEY_SM_FLAGS = "sm_flags";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -111,6 +114,12 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_KERNEL_VERSION, getFormattedKernelVersion());
         findPreference(KEY_KERNEL_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        findPreference(KEY_SM_AND).setEnabled(true);
+        findPreference(KEY_SM_KERNEL).setEnabled(true);
+        findPreference(KEY_SM_FLAGS).setEnabled(true);
+        setValueSummary(KEY_SM_AND, "ro.sm.android");
+        setValueSummary(KEY_SM_KERNEL, "ro.sm.kernel");
+        setValueSummary(KEY_SM_FLAGS, "ro.sm.flags");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
